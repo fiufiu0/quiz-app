bccbd53b3bbc501c9eca0a2f84a61f1adc2a1631
Łukasz <nipseyyo@gmail.com>
2021-07-31 17:30:17 +0200

    GitHub link, menu button disabled, css

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,15 +2,19 @@ import React, { useContext } from 'react';
 import { QuizContext } from "../helpers/Context";
 import "../App.css";
 
+
+
 function Menu() {
     const { gameState, setGameState, name, setName } = useContext(QuizContext);
 
     return (
         <div className="Menu">
-            <label className="Name">Player name:</label>
+            <label className="Name">Enter player name to play.</label>
             <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-            <button className="Button" onClick={() => { setGameState("quiz") }}>Start Quiz</button>
-        </div>
+            <button className="Button" onClick={() => { setGameState("quiz") }} disabled={name.length === 0}>Start Quiz</button>
+
+            <a href="https://github.com/lukaszgromadzki/quiz-app"><img src="https://i.imgur.com/mbDLKTY.png" alt="GitHub" /></a>
+        </div >
     )
 }
 
